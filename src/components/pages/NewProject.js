f2db63e2d039d7719ac1c35a08931069a172b3dc
JailsonA <maurocv18@gmail.com>
2023-02-06 10:54:53 +0000

    Criar Registo done

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -58,7 +58,7 @@ function NewProject() {
 
         {/* /.container-fluid */}
         <div className="row">
-          <div className="col-md-12">
+          <div className="col-md-8">
             <div className="card card-primary">
               <div className="card-header">
                 <h3 className="card-title">Formulario Projeto</h3>
@@ -77,6 +77,92 @@ function NewProject() {
               {/* /.card-body */}
             </div>
             {/* /.card */}
+          </div>
+          <div class="col-4" id="accordion">
+            <div class="card card-primary card-outline">
+              <a class="d-block w-100" data-toggle="collapse" href="#collapseOne">
+                <div class="card-header">
+                  <h4 class="card-title w-100">
+                    1. Descrição sobre o registo
+                  </h4>
+                </div>
+              </a>
+              <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                <div class="card-body">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                </div>
+              </div>
+            </div>
+            <div class="card card-primary card-outline">
+              <a class="d-block w-100" data-toggle="collapse" href="#collapseTwo">
+                <div class="card-header">
+                  <h4 class="card-title w-100">
+                    2. Nome
+                  </h4>
+                </div>
+              </a>
+              <div id="collapseTwo" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                  Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </div>
+              </div>
+            </div>
+            <div class="card card-primary card-outline">
+              <a class="d-block w-100" data-toggle="collapse" href="#collapseThree">
+                <div class="card-header">
+                  <h4 class="card-title w-100">
+                    3. Orçamento
+                  </h4>
+                </div>
+              </a>
+              <div id="collapseThree" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                  Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                </div>
+              </div>
+            </div>
+            <div class="card card-warning card-outline">
+              <a class="d-block w-100" data-toggle="collapse" href="#collapseFour">
+                <div class="card-header">
+                  <h4 class="card-title w-100">
+                    4. Categoria
+                  </h4>
+                </div>
+              </a>
+              <div id="collapseFour" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                  Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                </div>
+              </div>
+            </div>
+            <div class="card card-warning card-outline">
+              <a class="d-block w-100" data-toggle="collapse" href="#collapseFive">
+                <div class="card-header">
+                  <h4 class="card-title w-100">
+                    5. Serviço
+                  </h4>
+                </div>
+              </a>
+              <div id="collapseFive" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                  In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+                </div>
+              </div>
+            </div>
+            <div class="card card-warning card-outline">
+              <a class="d-block w-100" data-toggle="collapse" href="#collapseSix">
+                <div class="card-header">
+                  <h4 class="card-title w-100">
+                    6. Custo Serviço
+                  </h4>
+                </div>
+              </a>
+              <div id="collapseSix" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                  Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
